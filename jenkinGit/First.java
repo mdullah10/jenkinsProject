a694c87d933caebb16cb7f8b0a4456980c9f9a1f
mdUllah <mdulla1984@gmail.com>
2019-10-30 03:06:46 -0400

    first commit

---
+++ jenkinGit/First.java
@@ -0,0 +1,13 @@
+package jenkinGit;
+
+public class First {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=6;i++) {
+			System.out.println("This is My First project :"+i); 
+			
+		}
+
+	}
+
+}
